6ba0f2043aa2f15d197e9691059794ee892739cc
Seo Hyeon Bae <76519535+bread4614@users.noreply.github.com>
2022-02-22 19:14:33 +0900

    add: c06
    [complete] 100%

---
+++ C_06/ex03/ft_sort_params.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seohbae <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/15 12:14:42 by seohbae           #+#    #+#             */
+/*   Updated: 2022/02/17 13:35:14 by seohbae          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
+
+void	swap(char **s1, char **s2)
+{
+	char	*temp;
+
+	temp = *s1;
+	*s1 = *s2;
+	*s2 = temp;
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+void	ft_bubble_sort(int	argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+		{
+			swap(&argv[i], &argv[i + 1]);
+			i = 0;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	ft_bubble_sort(argc, argv);
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		i++;
+	}
+}
